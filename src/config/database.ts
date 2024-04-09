59a69c561b5399bf824f0de0b92049856cc672f1
Gatete Angelo Christian <ishgatetechristian@gmail.com>
2024-04-09 22:06:52 +0200

    Chore(Docker): Setting up docker configurations for backend application 1. Initializing a Dockerfile 2. Setting up a docker-compose.yaml file that runs two services [Delivers #187364723]

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,12 +1,21 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const port = process.env.PORT || 8080;
+const POSTGRES_USERNAME = process.env.POSTGRES_USERNAME 
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD
+const POSTGRES_DATABASE = process.env.POSTGRES_DATABASE 
+const POSTGRES_PORT = process.env.POSTGRES_PORT || 5432
 
 const sequelize = new Sequelize({
   dialect: 'postgres',
-  host: 'localhost',
+  host: 'database' || 'localhost',
   port: 5432,
-  username: 'postgres',
-  password: '12345',
-  database: 'postgres',
+  username: POSTGRES_USERNAME,
+  password: POSTGRES_PASSWORD,
+  database: POSTGRES_DATABASE,
 });
 
-export default  sequelize ;
+export default  sequelize ; 
+ 
