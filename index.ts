fde8ff6c79759b493c12baed03c07f187d37aad9
UwizeyimanaFurahaJustine <uwizefuraha@gmail.com>
2024-04-22 00:22:00 +0200

    chore-ESLint and pre commit hook #187364715
    Initialize Husky and add eslint script and ignore migrations dir
    
    Adding prettierrc and modify pre-commit
    
    Modify index.ts
    Rebase my branch with develop to get latest commits in develop branch
    
    modify package-lock.json
    
    Adding eslint configuration to lint ts file and remove unwanted files

--- index.ts
+++ index.ts
@@ -1,35 +1,37 @@
-import express from 'express';
-import http from 'http';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import compression from 'compression';
-import dotenv from 'dotenv';
-import swaggerUi from "swagger-ui-express";
-import specs from "./src/helpers/swagger";
+import express, { Request, Response } from 'express'
+import http from 'http'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import compression from 'compression'
+import dotenv from 'dotenv'
+import swaggerUi from 'swagger-ui-express'
+import specs from './src/helpers/swagger'
 
 dotenv.config()
-let port  = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
-export const  app = express();
+const app = express()
 // Serve Swagger UI
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
 
+app.use(
+  cors({
+    credentials: true,
+  }),
+)
 
-app.use(cors({
-    credentials:true,
-}))
+app.use(compression())
+app.use(bodyParser.json())
 
-app.use(compression()); 
-app.use(bodyParser.json());
+const server = http.createServer(app)
 
-const server = http.createServer(app);
-
-app.get('/welcome', (req:any, res:any)=>{
-  res.status(200).send({message:"Welcome to my API"})
+app.get('/welcome', (req: Request, res: Response) => {
+  res.status(200).send({ message: 'Welcome to my API' })
 })
-  server.listen(port, ()=>{
-    console.log(`listening on port https://localhost:${port}`)
-}) 
+server.listen(port, () => {
+  // console.log(`listening on port https://localhost:${port}`)
+})
 
+export default app
 // app.use('/', router())
-//startServer();
+// startServer();
--- test/dummy.test.ts
+++ test/dummy.test.ts
@@ -1,45 +1,36 @@
-import { expect } from 'chai';
+import { expect } from 'chai'
 import Calculator from '../src/dummy'
 
-describe("Test calculator", () => {
-    it("should return sum", ()=>{
-        //arrange
-        const calc = new Calculator();
+describe('Test calculator', () => {
+  it('should return sum', () => {
+    // act
+    const result = Calculator.add(2, 3)
 
-        // act
-        const result = calc.add(2,3);
+    // assert
+    expect(result).to.equal(5)
+  })
 
-        //assert
-        expect(result).to.equal(5);
-    })
-    it("should return subtract", ()=>{
-        //arrange
-        const calc = new Calculator();
+  it('should return subtract', () => {
+    // act
+    const result = Calculator.subtract(5, 3)
 
-        // act
-        const result = calc.substract(5,3);
+    // assert
+    expect(result).to.equal(2)
+  })
 
-        //assert
-        expect(result).to.equal(2);
-    })
-    it("should return product", ()=>{
-        //arrange
-        const calc = new Calculator();
+  it('should return product', () => {
+    // act
+    const result = Calculator.multiply(2, 3)
 
-        // act
-        const result = calc.multiply(2,3);
+    // assert
+    expect(result).to.equal(6)
+  })
 
-        //assert
-        expect(result).to.equal(6);
-    })
-    it("should return quotient", ()=>{
-        //arrange
-        const calc = new Calculator();
+  it('should return quotient', () => {
+    // act
+    const result = Calculator.divide(3, 3)
 
-        // act
-        const result = calc.divide(3,3);
-
-        //assert
-        expect(result).to.equal(1);
-    })
+    // assert
+    expect(result).to.equal(1)
+  })
 })
