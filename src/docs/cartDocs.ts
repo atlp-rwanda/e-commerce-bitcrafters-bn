60cb5076a230fe208eb2fc92cca5077248022c04
Gatete Ishema Angelo Christian <39400022+AngeloChristian1@users.noreply.github.com>
2024-05-30 07:20:59 +0200

    Feature(View cart): User can view tems in their cart (#74)
    1. User can view there cart
    
    [#Delivers 187364748]

--- src/docs/cartDocs.ts
+++ src/docs/cartDocs.ts
@@ -116,3 +116,103 @@
  *                   type: string
  *                   example: Internal server error
  */
+
+/**
+ * @swagger
+ * /cart:
+ *   get:
+ *     summary: Get user cart
+ *     description: Gets an active cart of a user
+ *     tags:
+ *       - CART
+ *     responses:
+ *       '201':
+ *         description: Cart retrived successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Cart retrived succssfully
+ *                 cart:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: string
+ *                       description: The ID of the cart
+ *                     buyerId:
+ *                       type: string
+ *                       description: The ID of the buyer
+ *                     items:
+ *                       type: array
+ *                       items:
+ *                         type: object
+ *                         properties:
+ *                           productId:
+ *                             type: string
+ *                             description: The ID of the product
+ *                           name:
+ *                             type: string
+ *                             description: The name of the product
+ *                           price:
+ *                             type: number
+ *                             description: The price of the product
+ *                           quantity:
+ *                             type: number
+ *                             description: The quantity of the product in the cart
+ *                           imageUrl:
+ *                             type: string
+ *                             description: The URL of the product image
+ *                     totalPrice:
+ *                       type: number
+ *                       description: The total price of the cart
+ *                     totalQuantity:
+ *                       type: number
+ *                       description: The total quantity of items in the cart
+ *                     status:
+ *                       type: string
+ *                       description: The status of the cart
+ *                       example: active
+ *       '400':
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Invalid quantity
+ *       '401':
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Unauthorized
+ *       '404':
+ *         description: Product not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Product not found
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Internal server error
+ */
